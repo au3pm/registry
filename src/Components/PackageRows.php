46c5024de17c6ce093f0822da07360a2ec933390
Anders Pedersen <hot415@hotmail.com>
2023-10-17 23:43:19 +0000

    Added support for searching for packages by name

--- src/Components/PackageRows.php
+++ src/Components/PackageRows.php
@@ -10,19 +10,20 @@ class PackageRows extends Component
     protected $properties = [
         'children' => null,
         'page' => 1,
+        'search' => null,
     ];
 
     protected function _render()
     {
         $children = $this->getProperty('children');
         if ($children === null) {
-            $children = Database::getInstance()->getPackages($this->getProperty('page'));
+            $children = Database::getInstance()->getPackages($this->getProperty('page'), $this->getProperty('search'));
         }
 
         for ($i = 0; $i < count($children); $i++) {
             $child = $children[$i];
             ?>
-            <tr <?php if ($i === count($children) - 1) { ?> hx-get="./packages/?page=<?=$this->getProperty('page') + 1?>" hx-trigger="revealed" hx-swap="afterend" <?php } ?>>
+            <tr <?php if ($i === count($children) - 1) { ?> hx-get="./packages/?page=<?=$this->getProperty('page') + 1?>&search=<?=$this->getProperty('search')?>" hx-trigger="revealed" hx-swap="afterend" <?php } ?>>
                 <td><a href="/package/<?=$child['id']?>/"><?=$child['name']?></a></td>
                 <td><a href="/user/<?=$child['owner_id']?>/"><?=$child['external_id']?></a></td>
             </tr>
